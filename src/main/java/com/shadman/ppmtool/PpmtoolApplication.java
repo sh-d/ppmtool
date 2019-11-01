c12d3aaee83d305fd99586aa17a7f4eb38bcca9a
sh-d <shadman005@gmail.com>
2019-11-01 21:15:52 +0530

    first commit 3

---
+++ src/main/java/com/shadman/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.shadman.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApplication.class, args);
+	}
+
+}
